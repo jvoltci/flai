d5bea4e032c9df5624359415dfa3a230168e2c7c
jvoltci <jvoltci@gmail.com>
2019-03-12 22:21:27 +0530

    Updates

--- precache-manifest.dc42aaa5e3339bf2f6bdcfca396b03f8.js
+++ precache-manifest.dc42aaa5e3339bf2f6bdcfca396b03f8.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "d2de45f826f11b1c3f22",
+    "revision": "50418484f88c79579ea8",
     "url": "/flai/static/css/main.f01ebbbf.chunk.css"
   },
   {
-    "revision": "d2de45f826f11b1c3f22",
-    "url": "/flai/static/js/main.d2de45f8.chunk.js"
+    "revision": "50418484f88c79579ea8",
+    "url": "/flai/static/js/main.50418484.chunk.js"
   },
   {
     "revision": "5aaa363694ede916b16b",
     "url": "/flai/static/js/runtime~main.5aaa3636.js"
   },
   {
-    "revision": "c88c70e5f4ff8bea6fac",
-    "url": "/flai/static/js/2.c88c70e5.chunk.js"
+    "revision": "7209c8fafb02ec7da57b",
+    "url": "/flai/static/js/2.7209c8fa.chunk.js"
   },
   {
     "revision": "9509d4cb62afce57add44831c5d39b9f",
     "url": "/flai/static/media/index.9509d4cb.jpg"
   },
   {
-    "revision": "5411c1a007799924dcf664e9eae19c0c",
+    "revision": "87e0d9304e5d1f373650b8fa429c1edb",
     "url": "/flai/index.html"
   }
 ];
