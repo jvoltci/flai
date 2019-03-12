ae73b47dac21840b1d34a813df3e064a2f7adf40
jvoltci <jvoltci@gmail.com>
2019-03-12 21:38:23 +0530

    Updates

---
+++ precache-manifest.89767334b6b01c725a0286f648fba27c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "09d5bc1826cf7a001ec9",
+    "url": "/static/css/main.fc18cf94.chunk.css"
+  },
+  {
+    "revision": "09d5bc1826cf7a001ec9",
+    "url": "/static/js/main.09d5bc18.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7209c8fafb02ec7da57b",
+    "url": "/static/js/2.7209c8fa.chunk.js"
+  },
+  {
+    "revision": "9509d4cb62afce57add44831c5d39b9f",
+    "url": "/static/media/index.9509d4cb.jpg"
+  },
+  {
+    "revision": "89da032c225d2885bfc1d7c286fb5a04",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/flai/precache-manifest.dc97ac5b7ba52b3a6958a50788a14bd3.js"
+  "/precache-manifest.89767334b6b01c725a0286f648fba27c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/flai/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
