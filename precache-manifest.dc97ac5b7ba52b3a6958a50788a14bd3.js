344581ac5c34804a2cf15121487b7461bcfa33b2
jvoltci <jvoltci@gmail.com>
2019-03-12 21:10:03 +0530

    Updates

---
+++ precache-manifest.dc97ac5b7ba52b3a6958a50788a14bd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b223144ecdb332e5071",
+    "url": "/flai/static/css/main.f01ebbbf.chunk.css"
+  },
+  {
+    "revision": "1b223144ecdb332e5071",
+    "url": "/flai/static/js/main.1b223144.chunk.js"
+  },
+  {
+    "revision": "5aaa363694ede916b16b",
+    "url": "/flai/static/js/runtime~main.5aaa3636.js"
+  },
+  {
+    "revision": "7209c8fafb02ec7da57b",
+    "url": "/flai/static/js/2.7209c8fa.chunk.js"
+  },
+  {
+    "revision": "9509d4cb62afce57add44831c5d39b9f",
+    "url": "/flai/static/media/index.9509d4cb.jpg"
+  },
+  {
+    "revision": "68acc7c84bbd7a90318f3318bd517e63",
+    "url": "/flai/index.html"
+  }
+];
