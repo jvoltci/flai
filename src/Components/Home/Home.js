f019665342c3eef05c35f7d11be17ce52dbceec3
jvoltci <jvoltci@gmail.com>
2019-05-19 17:58:45 +0530

    Done with Torrent Almost

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,7 +6,7 @@ const List = ({ items }) => (
   <ul className="ulist">
     <ul className="vlist">
       {
-        items.map((item) => <li className="litem"><a className="link" href={"https://flai-api.herokuapp.com/torrent/"+ item}>{item}</a></li>)
+        items.map((item, i) => <li key={i} className="litem"><a className="link" href={"https://flai-api.herokuapp.com/torrent/"+ item}>{item}</a></li>)
       }
     </ul>
   </ul>
@@ -47,14 +47,15 @@ class App extends Component {
           }})
         .then(res => res.json())
         .then(res => {
-          this.setState({files: res})
+          this.setState({files: res, list: 1, extension: ''})
         })
-        .then(() => {console.log(this.state.files); this.setState({list: 1})})
+        .catch(error => console.error('Error:', error));
   }
 
   componentDidUpdate() {
-    if(this.state.extension === "magnet")
-          this.handleTorrent();
+    if(this.state.extension === "magnet") {
+      this.handleTorrent();
+    }
   }
 
   render() {
@@ -63,7 +64,7 @@ class App extends Component {
         <h3 id="u1">Welcome To fl<span id="u11">ai</span> Downloads</h3>
         <div className="row">
           <div className="col-12" align="center">
-            <form onSubmit={e => this.state.extension==="magnet"?e.preventDefault():''} method="post" action="https://flai-api.heroku.com/download" >
+            <form onSubmit={e => this.state.extension==="magnet"?e.preventDefault():''} method="post" action="https://flai-api.herokuapp.com/download" >
               <div className="form-group">
                 <input onChange={(e) => this.changeURL(e)}
                  type="text" name="user[url]" required className="form-control" placeholder="Downloadable URL | Magnet URI" id="u2" />
@@ -75,15 +76,15 @@ class App extends Component {
                 <datalist id="extension" >
                   <option value=".zip">.zip</option>
                   <option value=".mp4">.mp4</option>
-                  <option value=".mkv">.mkv</option>
-                  <option value=".mp3">.mp3</option>
                   <option value=".exe">.exe</option>
                   <option value="magnet">magnet</option>
                 </datalist>
                 <button type="submit" className="btn btn-danger">Download</button>
               </div>
             </form>
+
             { this.state.list? <List items={this.state.files} />: '' }
+
           </div>
         </div>
       </div>
