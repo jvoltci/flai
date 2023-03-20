73f26bcf67c07306e573ccdbd15113fb3342964e
JAI <jvoltci@gmail.com>
2023-03-20 22:23:18 +0530

    Updates

---
+++ precache-manifest.d51f30cd615f57b1b89e2f26ed31d587.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "860668426e8aa92d1ddb",
+    "url": "/flai/static/css/main.5b574ef4.chunk.css"
+  },
+  {
+    "revision": "860668426e8aa92d1ddb",
+    "url": "/flai/static/js/main.84aa627d.chunk.js"
+  },
+  {
+    "revision": "d49b719ddede4328ad5d",
+    "url": "/flai/static/js/runtime~main.3f41be87.js"
+  },
+  {
+    "revision": "d0ff8212eb4516b8afcd",
+    "url": "/flai/static/js/2.f4c56b05.chunk.js"
+  },
+  {
+    "revision": "9509d4cb62afce57add44831c5d39b9f",
+    "url": "/flai/static/media/index.9509d4cb.jpg"
+  },
+  {
+    "revision": "15645076396f20e6f78b20fd92afe5b4",
+    "url": "/flai/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1dfd631d2e52ac340a7f374533ecd912.js"
+  "/flai/precache-manifest.d51f30cd615f57b1b89e2f26ed31d587.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/flai/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
